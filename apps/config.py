a0f1fed4b5d567f13959362db5f53da550ebb030
windy <yh1314529@qq.com>
2014-06-16 17:25:07 +0800

    The third version, finished all functions, need to be tested

--- apps/config.py
+++ apps/config.py
@@ -2,11 +2,11 @@ __author__ = 'windy'
 
 # debug = True
 debug = False
-# url_prefix = "http://ft.speiyou.com"
-# url_sso = "http://ljl.121learn.cn/auth/token/auto_login.php"
-# mails = ["ixdba_tuan800@163.com"]
-url_prefix = "http://wjiao.speiyou.cn"
-url_sso = "http://waijiao.speiyou.com/auth/token/auto_login.php"
-mails = ["windy.yang@huanxunedu.com", "cc.chen@huanxunedu.com",
-         "johnny.dai@huanxunedu.com", "simon.sun@huanxunedu.com"]
-
+url_prefix = "http://ft.speiyou.com"
+url_sso = "http://ljl.121learn.cn/auth/token/auto_login.php"
+mails = ["ixdba_tuan800@163.com"]
+# url_prefix = "http://wjiao.speiyou.cn"
+# url_sso = "http://waijiao.speiyou.com/auth/token/auto_login.php"
+# mails = ["windy.yang@huanxunedu.com", "cc.chen@huanxunedu.com",
+#          "johnny.dai@huanxunedu.com", "simon.sun@huanxunedu.com"]
+#
