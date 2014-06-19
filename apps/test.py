20cd7480e880f3f644f968e3274d96b916a8e5dc
windy <yh1314529@qq.com>
2014-06-19 23:00:19 +0800

    The third version, finished all functions, need to be tested

--- apps/test.py
+++ apps/test.py
@@ -4,8 +4,25 @@ __author__ = 'windy'
 
 from base import BaseHandler
 from utils import Route
+from utils import sendmail
+from http_msg import single_login
+from datetime import datetime
 
-@Route("/test/post")
+
+@Route("/test/uname")
 class TestPost(BaseHandler):
     def get(self, *args, **kwargs):
-        self.render("test_time_list.html")
+        uname = self.get_argument("uname")
+        uid = self.get_argument("uid")
+        if isinstance(uname, unicode):
+            uname = uname.encode("utf-8")
+            sso = single_login(uid, uname)
+            email = "Student From LJL id=%s, name=%s\n" \
+                    "Payed for:%s \n" \
+                    "Class Table Link: http://yueke.speiyou.com/timetable/list/moodle?uid=%s&claId=%s\n" \
+                    "SSO URL: %s" \
+                    % (uid, uname, "test 000", uid, "classId", sso)
+            sendmail(msg=email, subject="%s Student Pay For Class" % datetime.now())
+            self.write("world")
+        else:
+            self.write("Hello")
